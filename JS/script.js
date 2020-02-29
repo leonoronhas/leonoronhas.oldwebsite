418d870dd6fd9219dc3be7177e732462a7facea7
leonoronsantos <leosud099@gmail.com>
2020-02-28 22:11:45 -0700

    mobile menu created

--- JS/script.js
+++
@@ -1,9 +0,0 @@
-let contactImage = document.getElementById("Contact");
-
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
